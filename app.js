568c67aa5657e754480f57d68bc5303e14ff0a0a
PhamKun <tungmpham1996@gmail.com>
2022-01-06 18:34:37 -0600

    Workshop week 1

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var campsiteRouter = require('./routes/campsiteRouter')
+var partnerRouter = require('./routes/partnerRouter');
+var promotionsRouter = require('./routes/promotionRouter');
+
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
 
 var app = express();
 
@@ -21,6 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/partners', partnerRouter);
+app.use('/promotions', promotionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
